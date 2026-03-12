712c3a55b5199d5d7e12b6baa9c7ddb78ef4d168
Stepan Borisov <s.barysau@andersenlab.com>
2026-03-12 12:52:03 +0300

    Implement Extensions package

---
+++ ABC/Packages/Extensions/Sources/Extensions/Extension.CGFloat.swift
@@ -0,0 +1,15 @@
+//
+//  Extension.CGFloat.swift
+//  Extensions
+//
+//  Created by s.barysau on 12.03.26.
+//
+
+import Foundation
+
+public extension CGFloat {
+    var half: CGFloat {
+        self / 2
+    }
+}
+
